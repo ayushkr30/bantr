935adb669316ce96729f5ca3ae7e316fc5e82ef1
Ayush Kumar <ayushpal2017ak@gmail.com>
2025-08-27 03:41:28 +0530

    added git ignore files

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,14 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+
+const port = process.env.PORT;
 
 app.get('/', (req, res) => {
     res.send("Hello World");
 });
 
-app.listen(5001, ()=> console.log("Server is running on Port 5001"));
+app.listen(port, ()=> console.log("Server is running on Port 5001"));

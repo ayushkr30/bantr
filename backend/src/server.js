ad1f697c779a672cc522bbe7550f5d2389801277
Ayush Kumar <ayushpal2017ak@gmail.com>
2025-08-27 15:27:31 +0530

    connected DB and env,js config

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,19 @@
 import express from "express";
-import dotenv from "dotenv";
 
-dotenv.config();
+import { ENV } from "./config/env.js";
+import { connectDB } from "./config/db.js";
+
+
 
 const app = express();
 
-const port = process.env.PORT;
+
 
 app.get('/', (req, res) => {
     res.send("Hello World");
 });
 
-app.listen(port, ()=> console.log("Server is running on Port 5001"));
+app.listen(ENV.PORT, ()=> {
+    console.log("Server started on port:", ENV.PORT);
+    connectDB();
+});
